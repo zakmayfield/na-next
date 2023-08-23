ed3ca53f7d34c63ed5aad0502e42101e1191c7db
Zak Mayfield <zakmayfield@gmail.com>
2023-08-23 15:14:52 -0400

    feat: added company table & migrated

---
+++ prisma/migrations/20230823185821_company_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Company" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Company_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Company" ADD CONSTRAINT "Company_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
